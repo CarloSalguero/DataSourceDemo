fac40147c6822b34533ce1164a912ddd34469bc9
juego <sd0060032023@unab.edu.sv>
2024-08-26 22:18:20 -0600

    Guía IX : generando un modelo y extrayendo la información.

--- CapaConexion/CapaConexion/Form1.cs
+++ CapaConexion/CapaConexion/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaConexion.Modelos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -40,9 +41,25 @@ namespace CapaConexion
 
             SqlCommand comando = new SqlCommand();
             SqlDataReader reader = comando.ExecuteReader();
+
+            List <Customers > Customers = new List<Customers>();
+
             while (reader.Read())
             {
-                var customerId = reader[0];
+
+                Customers customers = new Customers();
+                customers.CompanyName = reader["CompanyName"] == DBNull.Value ? "" : (String)reader["CompanyName"];
+                customers.ContactName = reader["ContactName"] == DBNull.Value ? "" : (String)reader["ContactName"];
+                customers.ContactTitle = reader["ContactTitle"] == DBNull.Value ? "" : (String)reader["ContactTitle"];
+                customers.Address = reader["Address"] == DBNull.Value ? "" : (String)reader["Address"];
+                customers.City = reader["City"] == DBNull.Value ? "" : (String)reader["City"];
+                customers.Region = reader["Region"] == DBNull.Value ? "" : (String)reader["Region"];
+                customers.PostalCode = reader["PostalCode"] == DBNull.Value ? "" : (string)reader["PostalCode"];
+                customers.Country = reader["Country"] == DBNull.Value ? "" : (String)reader["Country"];
+                customers.Phone = reader["Phone"] == DBNull.Value ? "" : (String)reader["Phone"];
+                customers.Fax = reader["Fax"] == DBNull.Value ? "" : (String)reader["Fax"];
+
+                Customers.Add(customers);
             }
 
             MessageBox.Show("Conexion cerrada");
